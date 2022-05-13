322ff06b915efb8c9fc0209a138d7b1df7a77030
yarmall <andreiyarmolchuk@yandex.ru>
2022-05-13 11:59:50 +0400

    added validation for test

--- src/test/java/com/simbirsoft/tests/TextBoxTests.java
+++ src/test/java/com/simbirsoft/tests/TextBoxTests.java
@@ -14,6 +14,7 @@ public class TextBoxTests {
     @BeforeAll
     static void beforeAll() {
         Configuration.startMaximized = true;
+        Configuration.holdBrowserOpen = true;
         Configuration.baseUrl = "https://demoqa.com";
     }
 
@@ -22,8 +23,8 @@ public class TextBoxTests {
         String permanentAddress = "some street 1";
 
         open("/automation-practice-form");
-        $("#firstName").setValue("Andrey");
-        $("#lastName").setValue("Yarmolchuk");
+        $("#firstName").setValue("Ned");
+        $("#lastName").setValue("Stark");
         $("#userEmail").setValue("andrey@ss.com");
         $("#genterWrapper").$(new ByText("Male")).click();
         $("#userNumber").setValue("89876371777");
@@ -34,12 +35,23 @@ public class TextBoxTests {
         $("#subjectsInput").setValue("English").pressEnter();
         $("#hobbiesWrapper").$(new ByText("Music")).click();
         $("#uploadPicture").uploadFromClasspath("txt/test2.txt");
-        $("#currentAddress").setValue("some street 1");
+        $("#currentAddress").setValue("permanentAddress");
         $("#state").click();
         $("#stateCity-wrapper").$(new ByText("NCR")).click();
         $("#city").click();
         $("#stateCity-wrapper").$(new ByText("Noida")).click();
         $("#submit").click();
-    }
 
+        $(".modal-title").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").shouldHave(text("Ned Stark"));
+        $(".table-responsive").shouldHave(text("andrey@ss.com"));
+        $(".table-responsive").shouldHave(text("Male"));
+        $(".table-responsive").shouldHave(text("8987637177"));
+        $(".table-responsive").shouldHave(text("21 June,1996"));
+        $(".table-responsive").shouldHave(text("English"));
+        $(".table-responsive").shouldHave(text("Music"));
+        $(".table-responsive").shouldHave(text("test2.txt"));
+        $(".table-responsive").shouldHave(text("permanentAddress"));
+        $(".table-responsive").shouldHave(text("NCR Noida"));
+    }
 }
